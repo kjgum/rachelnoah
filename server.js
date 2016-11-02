a57a0dbe02ea60aeb2a00021d49593d5d20a38a0
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-11-02 12:38:38 -0400

    google maps wannabe

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(express.static(path.join(__dirname, 'public')));
-
+// routes
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
